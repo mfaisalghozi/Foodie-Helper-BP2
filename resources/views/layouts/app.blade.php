fbf91bdb28811448c0a6c12e8e76b7f67d4ee790
mfaisalghozi <mrafgaming@gmail.com>
2021-01-04 21:55:40 +0700

    reformat app blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>FoodieHelper - All In One About Food</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
